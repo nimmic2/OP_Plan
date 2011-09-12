adb12e6c3025c59375fea94652eb6b5250369a5f
Nimmi Carlose <beetles@iinet.net.au>
2011-09-12 19:34:41 +0800

    commit

---
+++ DBConn2.php
@@ -0,0 +1,16 @@
+<?php
+// setting
+/* ********** localhost ********* */
+define ( 'server', 'localhost' );
+define ( 'username', 'root' );
+define ( 'password', 'admin' );
+define ( 'database', 'operational_planning_system' );
+/* ****************************** */
+
+/* ********** Server ********* */
+//define ( 'server', 'medusa.murdoch.edu.au' );
+//define ( 'username', 'team01' );
+//define ( 'password', 'PETH90qa' );
+//define ( 'database', 'operational_planning_system' );
+/* ****************************** */
+?>
